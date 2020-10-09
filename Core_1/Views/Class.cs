4dece1a75c00eb9545f6b893b6fe3de536d04c0e
Алексей Малышев <akm@guap.ru>
2020-10-09 17:15:31 +0300

    added view classes

---
+++ Core_1/Views/Class.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core_1.Views
+{
+    public class EmployeeVeiwModel
+    {
+
+    }
+}
